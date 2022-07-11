12054f1ae93e19dfa59722343b0232c1721a4f36
aachbaro <aachbaro@student.42.fr>
2022-07-11 13:06:48 +0200

    yehyeh

---
+++ CPP06/ex01/INC/data.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   data.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aachbaro <aachbaro@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/11 12:02:18 by aachbaro          #+#    #+#             */
+/*   Updated: 2022/07/11 12:13:08 by aachbaro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DATA_HPP
+# define DATA_HPP
+# include <iostream>
+# include <stdint.h>
+
+typedef struct s_data {
+
+	int n;
+
+} t_data;
+
+#endif
